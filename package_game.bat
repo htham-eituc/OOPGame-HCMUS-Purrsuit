d0ed26cad5125a29d0ba86d443cc239c3afd2a60
htham-eituc <ngohuynhtham@gmail.com>
2025-08-26 12:23:48 +0700

    [Edit] pkg

--- package_game.bat
+++ package_game.bat
@@ -1,14 +1,26 @@
 @echo off
 setlocal
 
-:: Change this to your game exe name
+:: ================================
+:: CONFIGURATION
+:: ================================
+
+:: Your game exe name
 set EXE=Purrsuit.exe
 
 :: Where MinGW is installed (adjust to your system!)
 set MINGW=D:\msys64\mingw64\bin
 
+:: Where SDL2, SDL2_image, SDL2_mixer, SDL2_ttf are installed
+:: Typically this is the root of your SDL2 SDK, adjust to match yours
+set SDL2_PATH=D:\SDL2
+
 :: Output folder for packaging
 set OUT=dist
+
+:: ================================
+:: START PACKAGING
+:: ================================
 
 echo Creating package in %OUT%...
 
@@ -18,23 +30,29 @@ mkdir %OUT%
 mkdir %OUT%\assets
 
 :: Copy your exe
-copy %EXE% %OUT%\
+echo Copying game exe...
+copy "%EXE%" "%OUT%\" >nul
 
-:: Copy your assets (adjust path as needed)
-xcopy assets %OUT%\assets /E /I
+:: Copy your assets
+echo Copying assets...
+xcopy assets "%OUT%\assets" /E /I /Y >nul
 
 :: Copy required MinGW runtime DLLs
-copy %MINGW%\libgcc_s_seh-1.dll %OUT%\
-copy %MINGW%\libstdc++-6.dll %OUT%\
-copy %MINGW%\libwinpthread-1.dll %OUT%\
+echo Copying MinGW runtime DLLs...
+copy "%MINGW%\libgcc_s_seh-1.dll" "%OUT%\" >nul
+copy "%MINGW%\libstdc++-6.dll" "%OUT%\" >nul
+copy "%MINGW%\libwinpthread-1.dll" "%OUT%\" >nul
 
-:: Copy SDL2 DLLs (adjust path if you keep them elsewhere)
+:: Copy SDL2 DLLs
 echo Copying SDL2 DLLs...
-set SDL2_PATH=D:\sdl2
+copy "%SDL2_PATH%\bin\SDL2.dll"       "%OUT%\" >nul
+copy "%SDL2_PATH%\bin\SDL2_image.dll" "%OUT%\" >nul
+copy "%SDL2_PATH%\bin\SDL2_mixer.dll" "%OUT%\" >nul
+copy "%SDL2_PATH%\bin\SDL2_ttf.dll"   "%OUT%\" >nul
 
-copy "%SDL2_PATH%\bin\SDL2.dll"  %OUT%\
-copy "%SDL2_PATH%\bin\SDL2_image.dll"  %OUT%\
-copy "%SDL2_PATH%\bin\SDL2_mixer.dll"  %OUT%\
-copy "%SDL2_PATH%\bin\SDL2_ttf.dll"  %OUT%\
+echo.
+echo ======================================
+echo Packaging complete! Check the %OUT% folder.
+echo ======================================
 
 pause
